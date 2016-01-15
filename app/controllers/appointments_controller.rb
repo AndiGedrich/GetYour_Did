dbb6dc223061a3746f3913efa32cf22e709a2d62
Andrea Gedrich <andrea.d.amaro@gmail.com>
2016-01-14 22:14:14 -0800

    committing before attempting to add appointment functionality

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,12 @@
 class AppointmentsController < ApplicationController
 
+  def new
+    @technician_id = params[:id]
+    @appointment = Appointment.new
+  end
+
   def index
+    @appointments = appointment.booked == true
     if params[:query] && params[:search]
       @paginate = true
       search_by = params[:search].to_sym
@@ -12,19 +18,17 @@ class AppointmentsController < ApplicationController
           @appointments << appointment
         end
       end
+    else
+      @paginate = true
+      @appointments = Appointment.page(params[:page]).per(10).padding(0)
     end
-  else
-    @paginate = true
-    @courses = Course.page(params[:page]).per(10).padding(0)
-  end
-    @appointments = Appointment.all
-    @date = params[:month] ? Date.parse(params[:month]) : Date.today
   end
 
-  def new
-    @technician_id = params[:id]
-    @appointment = Appointment.new
-  end
+
+    #@appointments = Appointment.all
+    #@date = params[:month] ? Date.parse(params[:month]) : Date.today
+  #end
+
 
   def show
     @appointment = Appointment.find(params[:id])
@@ -33,6 +37,7 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.new(appointment_params)
     if @appointment.save
+      flash[:success] = "Added Appointment"
       redirect_to appointment_path(@appointment)
     else
       render :new
@@ -42,6 +47,37 @@ class AppointmentsController < ApplicationController
   def edit
     @appointment = Appointment.find(params[:id])
   end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update_attributes(appointment_params)
+      flash[:success] = "Updated Appointment"
+      redirect_to @appointment
+    else
+      render 'edit'
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def bookAppt
+    appointment = Appointment.find(params[:id][:booked])
+    user = current_user
+    appointment.users << user
+    flash[:success] = "Appointment Booked!"
+    params[:booked]=true
+    redirect_to appointments_path
+  end
+
+  def cancelAppt
+    appointment = Appointment.find(params[:id])
+    user = current_user
+    if appointment.users.where(:id => user.id).any?
+      appointment.users.delete(user.id)
+      params[:booked] = false
+
 
   def delete
     @appointment = Appointment.find(params[:id])
